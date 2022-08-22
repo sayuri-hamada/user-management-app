f6d26c8324bfb9d01d0cf528db422c9753683c03
sayuri.hamada <hamada@zizo.ne.jp>
2022-08-22 19:48:57 +0900

    Globalなスタイルを設定する

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import { Button, ChakraProvider } from '@chakra-ui/react';
-import './App.css';
+import theme from './theme/theme';
 
-function App() {
+export default function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Button colorScheme="teal">ボタン</Button>
+      <p>ああああああ</p>
     </ChakraProvider>
   );
 }
-
-export default App;
