a2978eff24016a89631833008f774c255653129e
Sayuri Hamada <110365626+sayuri-hamada@users.noreply.github.com>
2022-08-24 17:22:52 +0900

    ユーザー一覧取得機能の実装

--- src/components/pages/UserManagemen.tsx
+++ src/components/pages/UserManagemen.tsx
@@ -1,17 +1,42 @@
-import { Box, Stack, Wrap, WrapItem, Image, Text } from "@chakra-ui/react";
-import { memo, VFC } from "react";
+import {
+  Box,
+  Stack,
+  Wrap,
+  WrapItem,
+  Image,
+  Text,
+  Spinner,
+  Center
+} from "@chakra-ui/react";
+import { memo, useEffect, VFC } from "react";
 
 import { UserCard } from "../organisms/user/UserCard";
+import { useAllUsers } from "../../hooks/useAllUsers";
+
 export const UserManagement: VFC = memo(() => {
+  const { getUsers, users, loading } = useAllUsers();
+
+  useEffect(() => getUsers(), []);
+
   return (
-    <Wrap p={{ base: 4, md: 10 }}>
-      <WrapItem>
-        <UserCard
-          imageUrl="https://source.unsplash.com/random/"
-          userName="じゃけぇ"
-          fullName="aaaa aaaa"
-        />
-      </WrapItem>
-    </Wrap>
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users.map((user) => (
+            <WrapItem key={user.id} mx="auto">
+              <UserCard
+                imageUrl="https://source.unsplash.com/random/"
+                userName={user.username}
+                fullName={user.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
   );
 });
