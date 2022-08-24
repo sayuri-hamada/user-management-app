95afdbfd089a60d5e74a70e6f54b34b52b6def1b
Sayuri Hamada <110365626+sayuri-hamada@users.noreply.github.com>
2022-08-24 16:33:13 +0900

    ユーザー一覧画面の作成

--- src/components/pages/UserManagemen.tsx
+++ src/components/pages/UserManagemen.tsx
@@ -1,5 +1,17 @@
+import { Box, Stack, Wrap, WrapItem, Image, Text } from "@chakra-ui/react";
 import { memo, VFC } from "react";
 
+import { UserCard } from "../organisms/user/UserCard";
 export const UserManagement: VFC = memo(() => {
-  return <p>ユーザー管理ページです</p>
+  return (
+    <Wrap p={{ base: 4, md: 10 }}>
+      <WrapItem>
+        <UserCard
+          imageUrl="https://source.unsplash.com/random/"
+          userName="じゃけぇ"
+          fullName="aaaa aaaa"
+        />
+      </WrapItem>
+    </Wrap>
+  );
 });
